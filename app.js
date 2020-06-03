3e3a65c512aa15c41f0e85bcaff432916eca7160
duonglethanbinh <62211116+duonglethanbinh@users.noreply.github.com>
2020-06-04 00:20:04 +0700

    4

--- app.js
+++ app.js
@@ -62,9 +62,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
-
-const port = process.env.PORT || 5000
-
-app.listen(port, () => {
-  console.log(`Server Listening on ${port}`)
-});
+app.get('/', function (req, res) {
+  res.send('We are at server')
+})
+module.exports = app;
