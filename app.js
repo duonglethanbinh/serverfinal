3b7eed3955fef1777642a2ca3d199b399cb5d0ff
duonglethanbinh <62211116+duonglethanbinh@users.noreply.github.com>
2020-06-04 23:28:14 +0700

    9

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const cors = require('cors')
-
+const cors = require('cors');
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-
 const config = require("./config/key");
 
-// // Api docs
-// const YAML = require('yamljs');
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = YAML.load('./docs/docs.yaml');
-// const options = {
-//   customCss: '.swagger-ui .topbar { display: none }',
-//   customSiteTitle: "Renting",
-//   customfavIcon: "/docs/rent.png"
-// };
+// Api docs
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = YAML.load('./docs/docs.yaml');
+const options = {
+  customCss: '.swagger-ui .topbar { display: none }',
+  customSiteTitle: "Renting",
+  customfavIcon: "/docs/rent.png"
+};
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI,
+mongoose.connect(config.mongoURI,
   {
     useNewUrlParser: true, useUnifiedTopology: true,
     useCreateIndex: true, useFindAndModify: false
@@ -36,11 +34,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // support parsing of application/json type post data
 app.use(bodyParser.json());
 app.use(cookieParser());
-// app.use('/docs', express.static('docs'));
-// app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options, express.static('docs')));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/product', require('./routes/product'));
-
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use('/uploads', express.static('uploads'));
