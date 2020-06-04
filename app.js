4a0f0d0f9cca959f91db5a46eb757a09581a5fea
duonglethanbinh <62211116+duonglethanbinh@users.noreply.github.com>
2020-06-04 23:56:07 +0700

    14

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const app = express();
-// const path = require("path");
+const path = require("path");
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 
-// // Api docs
-// const YAML = require('yamljs');
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = YAML.load('./docs/docs.yaml');
-// const options = {
-//   customCss: '.swagger-ui .topbar { display: none }',
-//   customSiteTitle: "Renting",
-//   customfavIcon: "/docs/rent.png"
-// };
+// Api docs
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = YAML.load('./docs/docs.yaml');
+const options = {
+  customCss: '.swagger-ui .topbar { display: none }',
+  customSiteTitle: "Renting",
+  customfavIcon: "/docs/rent.png"
+};
 
 const mongoose = require("mongoose");
 mongoose.connect(config.mongoURI,
@@ -25,7 +25,7 @@ mongoose.connect(config.mongoURI,
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-// app.use(cors())
+app.use(cors())
 
 //to not get any deprecation warning or error
 //support parsing of application/x-www-form-urlencoded post data
@@ -36,23 +36,23 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 // app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options, express.static('docs')));
 app.use('/api/users', require('./routes/users'));
-// app.use('/api/product', require('./routes/product'));
+app.use('/api/product', require('./routes/product'));
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
-// app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static('uploads'));
 
 // Serve static assets if in production
-// if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
 
-//   // Set static folder   
-//   // All the javascript and css files will be read and served from this folder
-//   app.use(express.static("client/build"));
+  // Set static folder   
+  // All the javascript and css files will be read and served from this folder
+  app.use(express.static("client/build"));
 
-//   // index.html for all page routes    html or routing and naviagtion
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-//   });
-// }
+  // index.html for all page routes    html or routing and naviagtion
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
 app.get('/', function (req, res) {
   res.send('We are at server')
 })
