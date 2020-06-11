94b57344e1e0ad344be560c2732c3546cc526eb0
duonglethanbinh <62211116+duonglethanbinh@users.noreply.github.com>
2020-06-11 22:14:41 +0700

    27-test

--- server/routes/product.js
+++ server/routes/product.js
@@ -4,95 +4,125 @@ const { Product } = require("../models/Product");
 const multer = require('multer');
 const { auth } = require("../middleware/auth");
 var storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,'uploads/')
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/')
     },
-    filename:(req,file,cb)=>{
-        cb(null,`${Date.now()}_${file.originalname}`)
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}_${file.originalname}`)
     },
-    fileFilter:(req,file,cb)=>{
-        const ext=path.extname(file.originalname)
-        if (ext!=='.jpg'||ext!=='.png'){
-            return cb(res.status(400).end('Only jpg, png are allowed'),false);
+    fileFilter: (req, file, cb) => {
+        const ext = path.extname(file.originalname)
+        if (ext !== '.jpg' || ext !== '.png') {
+            return cb(res.status(400).end('Only jpg, png are allowed'), false);
         }
-        cb(null,true)
+        cb(null, true)
     }
 })
-var upload=multer({storage:storage}).single("file")
+var upload = multer({ storage: storage }).single("file")
 //=================================
 //             Product
 //=================================
 
 router.post("/uploadImage", auth, (req, res) => {
-    upload(req,res,err=>{
-        if(err) return res.json({success:false,err})
-        return res.json({success:true,image:res.req.file.path,fileName:res.req.file.filename})
+    upload(req, res, err => {
+        if (err) return res.json({ success: false, err })
+        return res.json({ success: true, image: res.req.file.path, fileName: res.req.file.filename })
     })
 });
 router.post("/uploadProduct", auth, (req, res) => {
-    const product=new Product(req.body)
-    product.save((err)=>{
-        if(err) returnres.status(400).json({success:false,err})
-        return res.status(200).json({success:true})
+    const product = new Product(req.body)
+    product.save((err) => {
+        if (err) returnres.status(400).json({ success: false, err })
+        return res.status(200).json({ success: true })
     })
 });
+
+router.patch('/updateProduct', auth, async (req, res) => {
+    try {
+        await Product.updateOne(
+            { _id: req.query._id },
+            {
+                $set: {
+                    title: req.body.title,
+                    description: req.body.description,
+                    price: req.body.price,
+                    images: req.body.images,
+                    devices: req.body.devices
+                },
+            },
+        );
+        res.json({ message: 'Updated' });
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+router.delete('/:deleteProduct', auth, async (req, res) => {
+    try {
+        await Product.deleteOne({ _id: req.query._id });
+        res.json({ message: 'Deleted' });
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
 router.post("/getProducts", (req, res) => {
-    let order=req.body.order ? req.body.order : "desc";
-    let sortBy=req.body.sortBy ? req.body.sortBy : "_id";
-    let limit=req.body.limit ? parseInt(req.body.limit) : 100;
-    let skip=parseInt(req.body.skip);
-    let findArgs={};
-    let term=req.body.searchTerm
-    for(let key in req.body.filters){
-        if(req.body.filters[key].length>0){
-            if(key==="price"){
-                findArgs[key]={
+    let order = req.body.order ? req.body.order : "desc";
+    let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    let skip = parseInt(req.body.skip);
+    let findArgs = {};
+    let term = req.body.searchTerm
+    for (let key in req.body.filters) {
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {
+                findArgs[key] = {
                     $gte: req.body.filters[key][0],
                     $lte: req.body.filters[key][1]
                 }
-            }else{
-                findArgs[key]=req.body.filters[key];
+            } else {
+                findArgs[key] = req.body.filters[key];
             }
         }
     }
-    if(term){
+    if (term) {
         Product.find(findArgs)
-        .find({$text:{$search:term}})
-        .populate("writer")
-        .sort([[sortBy,order]])
-        .skip(skip)
-        .limit(limit)
-        .exec((err,products)=>{
-            if(err) return res.status(400).json({success:false,err})
-            res.status(200).json({success:true,products,postSize:products.length})
-        })
-    }else{
+            .find({ $text: { $search: term } })
+            .populate("writer")
+            .sort([[sortBy, order]])
+            .skip(skip)
+            .limit(limit)
+            .exec((err, products) => {
+                if (err) return res.status(400).json({ success: false, err })
+                res.status(200).json({ success: true, products, postSize: products.length })
+            })
+    } else {
         Product.find(findArgs)
-        .populate("writer")
-        .sort([[sortBy,order]])
-        .skip(skip)
-        .limit(limit)
-        .exec((err,products)=>{
-            if(err) return res.status(400).json({success:false,err})
-            res.status(200).json({success:true,products,postSize:products.length})
-        })
+            .populate("writer")
+            .sort([[sortBy, order]])
+            .skip(skip)
+            .limit(limit)
+            .exec((err, products) => {
+                if (err) return res.status(400).json({ success: false, err })
+                res.status(200).json({ success: true, products, postSize: products.length })
+            })
     }
 });
 router.get("/products_by_id", (req, res) => {
-    let type=req.query.type
-    let productIds=req.query.id
-    if(type === "array"){
-        let ids=req.query.id.split(',');
-        productIds=[];
-        productIds=ids.map(item=>{
+    let type = req.query.type
+    let productIds = req.query.id
+    if (type === "array") {
+        let ids = req.query.id.split(',');
+        productIds = [];
+        productIds = ids.map(item => {
             return item
         })
     }
-    Product.find({'_id' :{$in:productIds}})
-    .populate('writer')
-    .exec((err,product)=>{
-        if(err) return req.status(400).send(err)
-        return res.status(200).send(product)
-    })
+    Product.find({ '_id': { $in: productIds } })
+        .populate('writer')
+        .exec((err, product) => {
+            if (err) return req.status(400).send(err)
+            return res.status(200).send(product)
+        })
 });
 module.exports = router;
